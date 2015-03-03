7b1d795d1ac06dd26efa2317773c833cb39c0088
Max Xu <deepthought@gmail.com>
2015-03-03 20:26:49 +0800

    update

--- server/server.js
+++ server/server.js
@@ -1,3 +1,18 @@
 Meteor.startup(function() {
-    Squares.remove({});
+   Meteor.call('reset');
+});
+
+Meteor.methods({
+    reset: function() {
+        Squares.remove({});
+
+        Squares.insert({
+            data: {},
+            view: module.view,
+            height: module.height,
+            width: module.width,
+            x: 1,
+            y: 1
+        });
+    }
 });
